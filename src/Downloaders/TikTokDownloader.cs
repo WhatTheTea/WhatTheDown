3475cd463a161c62726f5235ee12f62e0baff1c1
Alex <fox228.af@gmail.com>
2022-11-28 18:43:22 +0200

    TikTokDownloader

--- src/Downloaders/TikTokDownloader.cs
+++ src/Downloaders/TikTokDownloader.cs
@@ -1,18 +1,27 @@
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
+using WhatTheDown.Posts;
+
 namespace WhatTheDown.Downloaders;
 
 public sealed class TikTokDownloader : DownloaderBase
 {
 
     internal TikTokDownloader(HttpClient httpClient) :
-    base(httpClient, new(""))
+    base(httpClient, new("https://vm.tiktok.com/(.*)/"))
     { }
 
     internal override async Task OnUpdate(object? sender, (ITelegramBotClient botClient, Update update) e)
     {
-        
+        var message = e.update.Message;
+        var botClient = e.botClient;
+        var match = PostRegEx.Match(message?.Text ?? string.Empty).Value; // find url in message
+        if (match != string.Empty && message is not null)
+        {
+            var post = new TikTokPost(match);
+            await base.DownloadFile(botClient, message, post);
+        }
     }
 }
 public static class TikTokDownloaderExtensions
