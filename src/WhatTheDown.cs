123f6a6719d4520868de78d286dad5eb9836ead8
Alex <fox228.af@gmail.com>
2022-12-02 19:31:57 +0200

    moving to firebase

--- src/WhatTheDown.cs
+++ src/WhatTheDown.cs
@@ -1,6 +1,4 @@
 using System.Net;
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -8,19 +6,13 @@ using WhatTheDown.Downloaders;
 namespace WhatTheDown;
 public class WhatTheDown
 {
-    private readonly ILogger _log;
     private readonly IConfiguration _config;
     private readonly IHttpClientFactory _httpClientFactory;
-    public WhatTheDown(ILoggerFactory loggerFactory, IConfiguration config, IHttpClientFactory httpClientFactory)
+    public WhatTheDown(IConfiguration config, IHttpClientFactory httpClientFactory)
     {
-        _log = loggerFactory.CreateLogger<WhatTheDown>();
         _config = config;
         _httpClientFactory = httpClientFactory;
-    }
 
-    [Function("BotStart")]
-    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
-    {
         var botApiKey = _config["TGAPIKEY"] ?? throw new Exception("api key was not found");
 
         var httpClient = _httpClientFactory.CreateClient();
@@ -29,10 +21,6 @@ public class WhatTheDown
                      .AddRedditDownloader(httpClient)
                      .AddTikTokDownloader(httpClient);
 
-        var response = req.CreateResponse(HttpStatusCode.OK);
-        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-
-        response.WriteString("Bot has been started");
-        return response;
+        Console.WriteLine("Bot has been started");
     }
 }
